a547aeb5c0787f90b9392a21978f2ba9510e7c22
Rohaneasyfind <142909766+Rohaneasyfind@users.noreply.github.com>
2023-09-02 11:40:55 +0530

    Add files via upload

--- EF/includes/footer.php
+++ EF/includes/footer.php
@@ -7,15 +7,7 @@
 <div class="footer">
     <div class="page-container footer-container">
         <div class="footer-cities">
-            <div class="footer-city">
-                <a href="<?php echo $property_list_path.'?city=Delhi'; ?>">PG in Delhi</a>
-            </div>
-            <div class="footer-city">
-                <a href="<?php echo $property_list_path.'?city=Mumbai'; ?>">PG in Mumbai</a>
-            </div>
-            <div class="footer-city">
-                <a href="<?php echo $property_list_path.'?city=Bengaluru'; ?>">PG in Bangalore</a>
-            </div>
+            
             <div class="footer-city">
                 <a href="<?php echo $property_list_path.'?city=Hyderabad'; ?>">PG in Lucknow</a>
             </div>
